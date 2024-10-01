f3113bba71c065d9cbb4b6e6744f5b89c8f8f195
nanarhjass <nanarh1@icloud.com>
2024-09-30 23:34:34 -0400

    Update deploy.sh

--- k8s-manifests/deploy.sh
+++ k8s-manifests/deploy.sh
@@ -1,31 +1,6 @@
 #!/bin/bash
 
-# Check if kubectl is installed
-if ! command -v kubectl &> /dev/null; then
-    echo "Error: kubectl could not be found. Please install it and ensure it's in your PATH."
-    exit 1
-fi
-
-# Optionally set the context if needed
-# kubectl config use-context your-context-name
-
-# Deploy the application
-echo "Applying deployment manifest..."
-kubectl apply -f ./k8s-manifests/deployment.yaml --validate=false --insecure-skip-tls-verify
-
-# Check if the deployment was successful
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to apply deployment.yaml"
-    exit 1
-fi
-
-echo "Applying service manifest..."
-kubectl apply -f ./k8s-manifests/service.yaml --validate=false --insecure-skip-tls-verify
-
-# Check if the service was successful
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to apply service.yaml"
-    exit 1
-fi
-
-echo "Deployment completed successfully."
+kubectl apply -f namespace.yaml
+kubectl apply -f secret.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
